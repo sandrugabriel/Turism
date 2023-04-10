8da6fa203236498ed2b9f8b845f032bd907bc73c
Sandru Gabriel <gabi200611@yahoo.com>
2023-04-10 20:35:22 +0300

    done

--- Turism/Turism.cs
+++ Turism/Turism.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Turism
 {
-    public partial class Form1 : Form
+    public partial class Turism : Form
     {
-        public Form1()
+        public Turism()
         {
             InitializeComponent();
         }
